3239c3a67b0ca3e24ec20672a3636bb0892d473c
John <jsjshellz@gmail.com>
2021-06-09 11:16:53 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+  ] 
+
+
+function superbowlWin(array){
+  if (array.find(object => object.result === "W")){
+    return array.find(object => object.result === "W").year
+  }
+}
+
+
+ 
+
+
 
